2cea63fee3f948e971f6be794f3e3524aa5f33f3
marlens123 <marlena@itdex.de>
2026-02-01 18:19:38 -0500

    environment setup

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -3,7 +3,11 @@
 #SBATCH --mem=8G
 #SBATCH --error=error.txt
 
-conda activate /home/slurm/comp597/conda/envs/comp597
+# environment setup
+SCRIPTS_DIR=${COMP597_SLURM_SCRIPTS_DIR:-$(readlink -f -n $(dirname $0))}
+. ${SCRIPTS_DIR}/conda_init.sh
+conda activate ${COMP597_JOB_CONDA_ENV_PREFIX}
+
 cd /mnt/teaching/slurm/mreil2
 
 hf download InfImagine/FakeImageDataset \
