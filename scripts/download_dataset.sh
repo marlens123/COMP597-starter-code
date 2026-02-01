08b09248af340d4e4b7c736fe354102487bcf108
marlens123 <marlena@itdex.de>
2026-02-01 17:15:23 -0500

    download subset

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --time=1:00:00
 #SBATCH --mem=8G
+#SBATCH --error=error.txt
 
+conda activate /home/slurm/comp597/conda/envs/comp597
 cd /mnt/teaching/slurm/mreil2
-git lfs install
-git clone https://huggingface.co/datasets/InfImagine/FakeImageDataset
-cd FakeImageDataset
-git lfs pull
+
+hf download InfImagine/FakeImageDataset \
+  --repo-type dataset \
+  --include "ImageData/val/SDv21-CC15K/*"
