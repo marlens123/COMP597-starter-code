4c68b79af9bc2ebf3dade2879c1419e2d0c29c8e
marlens123 <marlena@itdex.de>
2026-02-02 18:00:01 -0500

    adjust data path

--- src/models/resnet152/resnet152.py
+++ src/models/resnet152/resnet152.py
@@ -89,7 +89,7 @@ def resnet152_init(conf: config.Config, dataset: torch.utils.data.Dataset) -> Tu
         batch_size = conf.batch_size
         loader = "pytorch"
         num_workers = 0
-        data = "data/fakeimagenet"
+        data = "data/fakeimagenet/FakeImageNet"
 
     dataloader_args = DataloaderArgs()
 
