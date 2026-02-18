15e8bb5fba8e77c7291e00f0f1df1e9ceb738a6a
marlens123 <marlena@itdex.de>
2026-02-17 20:58:08 -0500

    change number of workers to 0

--- src/config/data/fakeimagenet/fakeimagenet_config.py
+++ src/config/data/fakeimagenet/fakeimagenet_config.py
@@ -7,4 +7,4 @@ class DataConfig(_BaseConfig):
         self._arg_folder = _Arg(type=str, help="The storage folder of the dataset.", default="")
         self._arg_loader = _Arg(type=str, help="Which dataloader to use.", default="pytorch")
         self._arg_split = _Arg(type=str, help="How to split the dataset (ex: train[:100])", default="")
-        self._arg_load_num_proc = _Arg(type=int, help="Number of threads used to load the dataset.", default=1)
+        self._arg_load_num_proc = _Arg(type=int, help="Number of threads used to load the dataset.", default=0)
