b49b06c75c62b463ac349d6d60f13f82dc149a68
marlens123 <marlena@itdex.de>
2026-04-02 13:02:02 -0400

    small script

---
+++ scripts/start-resnet152-small.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+SCRIPTS_DIR=$(readlink -f -n $(dirname $0))
+REPO_DIR=$(readlink -f -n ${SCRIPTS_DIR}/..)
+
+
+### run ResNet152 Basic Resource Trainer 3x and with different batch sizes
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 32 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats basic_resources_stats \
+    --trainer_configs.basic_resources.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/basic_resources_logs'
+
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 64 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats basic_resources_stats \
+    --trainer_configs.basic_resources.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/basic_resources_logs'
+
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 128 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats basic_resources_stats \
+    --trainer_configs.basic_resources.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/basic_resources_logs'
+
+### run ResNet152 with CodeCarbon tracking 3x and for different batch sizes
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 32 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats codecarbon_resnet \
+    --trainer_stats_configs.codecarbon.run_num 1 \
+    --trainer_stats_configs.codecarbon.project_name test \
+    --trainer_stats_configs.codecarbon.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/codecarbonlogs'
+
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 64 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats codecarbon_resnet \
+    --trainer_stats_configs.codecarbon.run_num 2 \
+    --trainer_stats_configs.codecarbon.project_name test \
+    --trainer_stats_configs.codecarbon.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/codecarbonlogs'
+
+${SCRIPTS_DIR}/srun.sh \
+    --logging.level INFO \
+    --model resnet152 \
+    --data fakeimagenet \
+    --trainer resnet_simple \
+    --batch_size 128 \
+    --learning_rate 1e-6 \
+    --data_configs.fakeimagenet.folder '${COMP597_JOB_STUDENT_STORAGE_DIR}/fakeimagenet/FakeImageNet/train' \
+    --trainer_stats codecarbon_resnet \
+    --trainer_stats_configs.codecarbon.run_num 3 \
+    --trainer_stats_configs.codecarbon.project_name test \
+    --trainer_stats_configs.codecarbon.output_dir '${COMP597_JOB_STUDENT_STORAGE_DIR}/resnet/codecarbonlogs'
